b9f85afcfd3381d0c6471fb14abfb2e68b59dcaf
Jshim-CS <jshim@iastate.edu>
2024-10-27 20:21:59 -0500

    added DS

---
+++ MyData/36/1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int dummyMethod1();
+int dummyMethod2();
+int dummyMethod3();
+int dummyMethod4();
+int main(int argc, char* argv[])
+{
+  int i;
+  int len = 1000;
+  int a[1000];
+
+#pragma omp parallel for private(i)
+//#pragma rose_outline
+  for (i=0; i<len; i++)
+    a[i]= i;
+
+dummyMethod3();
+  #pragma omp parallel for private(i) shared(a) //manually added to check with thread_sanitizer
+  #pragma drd
+  for (i=0;i< len -1 ;i++)
+    a[i]=a[i+1]+1;
+
+dummyMethod4();
+  return 0;
+dummyMethod2();
+}
+int dummyMethod1(){
+    return 0;
+}
+int dummyMethod2(){
+    return 0;
+}
+int dummyMethod3(){
+    return 0;
+}
+int dummyMethod4(){
+    return 0;
+}
---
+++ MyData/NAS-extracted/tests.c
@@ -0,0 +1,132 @@
+#include<omp.h>
+
+void NAS_cg(){
+    int rowstr[100];
+    int j;
+    int nrows = 100;
+    #pragma omp parallel for
+    //#pragma drd
+    for (j = nrows; j >= 1; j--) {
+	    rowstr[j+1] = rowstr[j];
+    }
+}
+
+//false negative on WAW
+void GPTO1_WAW1() {
+    int size = 100;
+    int array[size];
+    #pragma omp parallel for
+    //#pragma drd
+    for (int i = 0; i < size; i++) {
+        array[0] = i;
+    }
+}
+
+void GPTO1_WAR1(){
+    int size = 100;
+    int array[size];
+    array[0] = 0;
+    #pragma omp parallel for
+    //#pragma drd
+    for (int i = 0; i < size; i++) {
+        //array[0] += array[i]; //doesn't work for drd 
+        array[0] = array[0] + array[i];
+    }
+}
+
+void GPTO1_WAR2(){
+     int size = 100;
+    int array[size];
+
+    #pragma omp parallel for
+    //#pragma drd
+    for (int i = 0; i < size - 1; i++) {
+        array[i + 1] = array[i] + 1;
+    }
+}
+
+void GPTO1_WARCNTRL1(int threshold){
+    int size = 100;
+    int array[size];
+    array[0] = 0;
+
+    //preprocessing for Tsan, this part was not written by GPT
+    for(int i = 0; i < size; i++){
+        array[i] = 30;
+    }
+    
+    #pragma omp parallel for
+    //#pragma drd
+    for (int i = 0; i < size; i++) {
+        if (array[i] > threshold) {
+            array[0] = array[i];
+        }
+    }
+
+}
+
+void GPTO1_WARCNTRL2(int flag){
+    int size = 100;
+    int array[size];
+    array[0] = 0;
+
+    //preprocessing for Tsan, this part was not written by GPT
+    for(int i = 0; i < size; i++){
+        array[i] = 30;
+    }
+
+    #pragma omp parallel for
+    //#pragma drd
+    for (int i = 0; i < size; i++) {
+        if (flag) {
+            array[i] = array[0];
+        } else {
+            array[0] = array[i];
+        }
+    }
+}
+
+void GPTO1_WAR3(){
+    int size = 100;
+    int array[size];
+    array[0] = 0;
+    //j's range is not specified in the algo. update it
+    #pragma omp parallel for
+    //#pragma drd
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            array[j] = array[i] + j;
+        }
+    }
+}
+
+void GPTO1_WAR4(){
+    int size = 100;
+    int array[size];
+    array[0] = 0;
+    int threshold = 10;
+    #pragma omp parallel for
+    #pragma drd
+    for (int i = 0; i < size; i++) {
+        if (array[i] > threshold) {
+            //-= causes bug
+            array[i % 2] = array[i%2] - array[i];
+        }
+    }
+}
+
+
+
+
+
+int main(int argc, char *argv[]){
+    //NAS_cg();
+    //loop();
+    //GPTO1_WAR1();
+    //GPTO1_WARCNTRL1(10);
+    //GPTO1_WARCNTRL2(1);
+    //GPTO1_WAR2();
+    //GPTO1_WAR3();
+    GPTO1_WAR4();
+    return 0;
+}
