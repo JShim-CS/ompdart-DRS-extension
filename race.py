56b25838875d6d265e2c49e985cd1f6237490500
Jshim-CS <jshim@iastate.edu>
2024-11-17 20:03:04 -0600

    ran tests

---
+++ race.py
@@ -0,0 +1,184 @@
+# import os
+# import re
+
+# def replace_omp_pragmas(source_dir, dest_dir):
+#     """
+#     Replace `#pragma omp` directives (e.g., `#pragma omp parallel for private(...)`) with `#pragma drd` in `.c` and `.cpp` files.
+#     Save the modified files in the specified destination directory.
+
+#     Args:
+#         source_dir (str): Path to the directory containing the source files.
+#         dest_dir (str): Path to the directory to save the modified files.
+#     """
+#     # Ensure the destination directory exists
+#     os.makedirs(dest_dir, exist_ok=True)
+
+#     # Regular expression to match the entire OpenMP pragma line
+#     omp_pattern = re.compile(r"#pragma omp.*")
+
+#     # Walk through the source directory
+#     for root, _, files in os.walk(source_dir):
+#         for file in files:
+#             if file.endswith(".c") or file.endswith(".cpp"):
+#                 # Full path to the source file
+#                 source_file_path = os.path.join(root, file)
+
+#                 # Read the file content
+#                 with open(source_file_path, 'r') as source_file:
+#                     content = source_file.read()
+
+#                 # Replace the entire OpenMP pragma line with #pragma drd
+#                 modified_content = omp_pattern.sub("#pragma drd", content)
+
+#                 # Construct the destination file path
+#                 relative_path = os.path.relpath(root, source_dir)
+#                 dest_file_dir = os.path.join(dest_dir, relative_path)
+#                 os.makedirs(dest_file_dir, exist_ok=True)
+#                 dest_file_path = os.path.join(dest_file_dir, file)
+
+#                 # Write the modified content to the destination file
+#                 with open(dest_file_path, 'w') as dest_file:
+#                     dest_file.write(modified_content)
+
+#                 print(f"Processed: {source_file_path} -> {dest_file_path}")
+
+# # Example usage
+# source_directory = "/programming/dataracebench/micro-benchmarks"
+# destination_directory = "/programming/bench"
+# replace_omp_pragmas(source_directory, destination_directory)
+
+# import os
+# import re
+
+# def delete_files_with_multiple_pragmas(source_dir):
+#     """
+#     Find and delete all `.c` and `.cpp` files in the source directory that contain at least two `#pragma drd` directives.
+
+#     Args:
+#         source_dir (str): Path to the directory containing the source files.
+#     """
+#     # Regular expression to match `#pragma drd`
+#     pragma_pattern = re.compile(r"#pragma drd")
+
+#     # Walk through the source directory
+#     for root, _, files in os.walk(source_dir):
+#         for file in files:
+#             if file.endswith(".c") or file.endswith(".cpp"):
+#                 # Full path to the source file
+#                 source_file_path = os.path.join(root, file)
+
+#                 # Read the file content
+#                 with open(source_file_path, 'r') as source_file:
+#                     content = source_file.read()
+
+#                 # Count occurrences of `#pragma drd`
+#                 pragma_count = len(pragma_pattern.findall(content))
+
+#                 if pragma_count >= 2:
+#                     # Delete the file
+#                     os.remove(source_file_path)
+#                     print(f"Deleted: {source_file_path}")
+
+# # Example usage
+# source_directory = "/programming/bench"
+# delete_files_with_multiple_pragmas(source_directory)
+
+
+# import os
+# import shutil
+
+# def copy_common_files(path1, path2, path3):
+#     """
+#     Copy files with the same name in both `path1` and `path2` to `path3`,
+#     taking the files from `path2`.
+
+#     Args:
+#         path1 (str): Path to the first directory.
+#         path2 (str): Path to the second directory.
+#         path3 (str): Path to the destination directory.
+#     """
+#     # Ensure the destination directory exists
+#     os.makedirs(path3, exist_ok=True)
+
+#     # Get the set of file names in path1 and path2
+#     files_in_path1 = {f for f in os.listdir(path1) if f.endswith(".c") or f.endswith(".cpp")}
+#     files_in_path2 = {f for f in os.listdir(path2) if f.endswith(".c") or f.endswith(".cpp")}
+
+#     # Find common files between the two paths
+#     common_files = files_in_path1.intersection(files_in_path2)
+
+#     # Copy common files from path2 to path3
+#     for file_name in common_files:
+#         src_file = os.path.join(path2, file_name)
+#         dest_file = os.path.join(path3, file_name)
+#         shutil.copy2(src_file, dest_file)
+#         print(f"Copied: {src_file} -> {dest_file}")
+
+# # Example usage
+# path1 = "/programming/bench"
+# path2 = "/programming/dataracebench/micro-benchmarks"
+# path3 = "/programming/subsetParallel"
+# copy_common_files(path1, path2, path3)
+
+
+import os
+import subprocess
+import csv
+
+def run_sh_and_log_results(c_path, sh_script, output_csv):
+    """
+    Run `./run.sh` for each `.c` file in a directory and log the results to a CSV.
+
+    Args:
+        c_path (str): Path containing `.c` files.
+        sh_script (str): Path to the `run.sh` script.
+        output_csv (str): Path to the output CSV file.
+    """
+    # Ensure the output directory for the CSV exists
+    os.makedirs(os.path.dirname(output_csv), exist_ok=True)
+
+    # List all `.c` files in the directory
+    c_files = [os.path.join(c_path, f) for f in os.listdir(c_path) if f.endswith('.c')]
+
+    # Open CSV file for writing results
+    with open(output_csv, mode='w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(["Input File", "Result"])
+
+        # Process each `.c` file
+        for c_file in c_files:
+            try:
+                # Run the shell script
+                result = subprocess.run([sh_script, "-i", c_file, "-o", "test2.c"],
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE,
+                                        text=True)
+
+                # Combine stdout and stderr to capture all output
+                combined_output = result.stdout + result.stderr
+
+                # Parse the output
+                if "seems like data race(waw/raw/war) exists within the loop!" in combined_output:
+                    csv_writer.writerow([c_file, "true"])
+                elif "seems like no data race exists (please double check)" in combined_output:
+                    csv_writer.writerow([c_file, "false"])
+                else:
+                    csv_writer.writerow([c_file, "fail"])
+
+                print(f"Processed: {c_file}")
+
+            except Exception as e:
+                # Handle any unexpected errors
+                print(f"Error processing {c_file}: {e}")
+                csv_writer.writerow([c_file, "fail"])
+
+
+
+
+# Example usage
+c_files_path = "/programming/bench-drd"
+run_sh_script = "./run.sh"
+output_csv_path = "./drsBench.csv"
+
+run_sh_and_log_results(c_files_path, run_sh_script, output_csv_path)
+
--- test2.c
+++ test2.c
@@ -1,44 +1,74 @@
 /*
-!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!
-!!! Copyright (c) 2017-20, Lawrence Livermore National Security, LLC
-!!! and DataRaceBench project contributors. See the DataRaceBench/COPYRIGHT file for details.
-!!!
-!!! SPDX-License-Identifier: (BSD-3-Clause)
-!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!
- */
+Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+Produced at the Lawrence Livermore National Laboratory
+Written by Chunhua Liao, Pei-Hung Lin, Joshua Asplund,
+Markus Schordan, and Ian Karlin
+(email: liao6@llnl.gov, lin32@llnl.gov, asplund1@llnl.gov,
+schordan1@llnl.gov, karlin1@llnl.gov)
+LLNL-CODE-732144
+All rights reserved.
+
+This file is part of DataRaceBench. For details, see
+https://github.com/LLNL/dataracebench. Please also see the LICENSE file
+for our additional BSD notice.
+
+Redistribution and use in source and binary forms, with
+or without modification, are permitted provided that the following
+conditions are met:
+
+* Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the disclaimer below.
+
+* Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the disclaimer (as noted below)
+  in the documentation and/or other materials provided with the
+  distribution.
+
+* Neither the name of the LLNS/LLNL nor the names of its contributors
+  may be used to endorse or promote products derived from this
+  software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL LAWRENCE LIVERMORE NATIONAL
+SECURITY, LLC, THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 /*
- * This is a program based on a dataset contributed by
- * Wenhao Wu and Stephen F. Siegel @Univ. of Delaware.
+Matrix-vector multiplication: outer-level loop parallelization
+*/
+#define N 100
 
- * No race. The array b is divided into two non-overlapping halves that are referenced by u[0] and u[1].
- */
-
-#include <stdlib.h>
-#include <stdio.h>
-
-double c = 0.2;
-int n = 20, nsteps = 100;
+double a[N][N],v[N],v_out[N];
+int mv()
+{           
+  int i,j;
+#pragma drd
+  for (i = 0; i < N; i++)
+  {         
+    float sum = 0.0;
+    for (j = 0; j < N; j++)
+    { 
+      sum += a[i][j]*v[j];
+    }  
+    v_out[i] = sum;
+  }         
+  return 0; 
+}
 
 int main()
 {
-  double *b = malloc(2 * n * sizeof(double));
-  double *u[2] = {&b[0], &b[n]};
-  for (int i = 1; i < n - 1; i++)
-    u[0][i] = u[1][i] = 1.0 * rand() / RAND_MAX;
-  u[0][0] = u[0][n - 1] = u[1][0] = u[1][n - 1] = 0.5;
-  int p = 0;
-  for (int t = 0; t < nsteps; t++)
-  {
-#pragma drd
-    for (int i = 1; i < n - 1; i++)
-    {
-      u[1 - p][i] = u[p][i] + c * (u[p][i - 1] + u[p][i + 1] - 2 * u[p][i]);
-    }
-    p = 1 - p;
-  }
-  for (int i = 0; i < n; i++)
-    printf("%1.2lf ", u[p][i]);
-  printf("\n");
-  free(b);
+  mv();
+  return 0;
 }
+
